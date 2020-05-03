c8334f396e9687705233c13ec4427992fc1a4086
yash8997 <thayash901@gmail.com>
2020-05-03 08:22:13 +0530

    add modal

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
     $(".p1").show();
     $(".p2").hide();
     $(".p3").hide();
+  //  $(".popup").hide();
 $(".collaps1").click(()=>{
     $(".p1").toggle();
     $(".p2").hide();
     $(".p3").hide();
 })
-
 $(".collaps2").click(()=>{
     $(".p2").toggle();
     $(".p1").hide();
@@ -19,6 +19,15 @@ $(".collaps3").click(()=>{
     $(".p2").hide();
     $(".p1").hide();
 
+})
+$(".demo").click(()=>{
+    $(".popup").fadeIn(); 
+})
+$(".clsoe").click(()=>{
+    $(".popup").fadeOut(); 
+})
+$(".closebutton").click(()=>{
+    $(".popup").fadeOut(); 
 })
 if($(window).width() < 767)
 {
